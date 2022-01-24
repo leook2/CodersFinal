c777db2f947a763381696ad6e1ca51eaf844c6d9
Leonardo <leook2@gmail.com>
2022-01-23 23:08:30 -0300

    ultimaAtualizacao

---
+++ Desafio/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Desafio.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Desafio.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Despesa> Despesas { get; set; }
+        public DbSet<Morador> Moradores { get; set; }
+        public DbSet<Unidade> Unidades { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Despesa>().HasKey(m => m.idDespesa);
+            base.OnModelCreating(builder);
+
+            builder.Entity<Inquilino>().HasKey(m => m.idInquilino);
+            base.OnModelCreating(builder);
+
+            builder.Entity<Unidade>().HasKey(m => m.idUnidade);
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Desafio/Model/Morador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Model
+{
+    public class Morador
+    {
+        public int IdMorador { get; set; }
+        public string name { get; set; }
+        public int idade { get; set; }
+        public string sexo { get; set; }
+        public int telefone { get; set; }
+        public string email { get; set; }
+    }
+}
